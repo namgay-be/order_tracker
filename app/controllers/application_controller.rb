133ea55010b40cc36223052b400f9f436e92a9a1
Tashi Dendup <tashi.dendup@dcpl.bt>
2019-12-26 11:57:21 +0600

    Basic setup for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include Pundit
+  include Renderer
+  include Pagination
+  include PreFilters
+  include Pagy::Backend
+  include ApiErrors::ErrorHandler
+  include ActionController::Cookies
+  include HttpAcceptLanguage::AutoLocale
 end
