be0aeeb42fba0701fb3a8a7ca7d51e8e9c359aef
namgay <Namgay Tenzin>
2020-04-27 22:47:57 +0600

    cryo serializer fix

--- app/serializers/cryo_serializer.rb
+++ app/serializers/cryo_serializer.rb
@@ -1,2 +1,27 @@
-class CryoSerializer < SeedSerializer
+class CryoSerializer < ApplicationSerializer
+  key :seed
+  collection_key :seeds
+  serialize(
+    :id,
+    :type,
+    :crop_name,
+    :family,
+    :genus,
+    :species,
+    :sub_texa,
+    :material_type,
+    :classification,
+    :resistant,
+    :susceptible,
+    :seed_status,
+    :characteristics,
+    :requires_multiplication
+  )
+
+  serialize :collection_info, with: CollectionInfoSerializer
+  serialize :cultivation_info, with: CultivationInfoSerializer
+  serialize :donor_info, with: DonorInfoSerializer
+  serialize :donor_field_info, with: DonorFieldInfoSerializer
+  serialize :seed_info, with: SeedInfoSerializer
+  serialize :test_details, with: TestDetailSerializer
 end
