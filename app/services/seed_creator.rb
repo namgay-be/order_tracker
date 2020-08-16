dea694dc023c349a092d0d528775db87296c2763
namgay <Namgay Tenzin>
2020-08-17 01:10:59 +0600

    code refactor

--- app/services/seed_creator.rb
+++ app/services/seed_creator.rb
@@ -5,11 +5,11 @@ class SeedCreator < ApplicationForm
     @seed = Seed.new(params)
     @seed.assign_attributes(creator: current_user)
     @donor = DonorInfo.new(donor_params) if donor_params.present?
-
     ActiveRecord::Base.transaction do
       donor.save! if donor.present?
       seed.save!
       seed.update_column(:donor_info_id, donor.id) if donor.present?
+      seed
     end
   end
 end
